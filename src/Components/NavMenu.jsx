69290aa0ce69731b7b5044d10038d0f7d9b9eea0
mehedititu <mh.me.mehedi@gmail.com>
2023-02-10 16:35:38 +0600

    UI done

---
+++ src/Components/NavMenu.jsx
@@ -0,0 +1,124 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+    const [navbar, setNavbar] = useState(false);
+
+    return (
+        <nav className="w-full bg-purple-500 shadow">
+            <div className="justify-between px-4 mx-auto lg:container md:items-center md:flex md:px-8">
+                <div>
+                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
+                        <Link to="/">
+                            <h2 className="text-2xl font-bold text-white">
+                                LOGO
+                            </h2>
+                        </Link>
+                        <div className="md:hidden">
+                            <button
+                                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                                onClick={() => setNavbar(!navbar)}
+                            >
+                                {navbar ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6 text-white"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6 text-white"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth={2}
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div
+                        className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                            navbar ? "block" : "hidden"
+                        }`}
+                    >
+                        {/* <ul
+                            className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0"
+                            onClick={() => setNavbar(!navbar)}
+                        >
+                            <li className="text-white hover:text-indigo-200">
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li className="text-white hover:text-indigo-200">
+                                <Link to="/blog">Blog</Link>
+                            </li>
+                            <li className="text-white hover:text-indigo-200">
+                                <Link to="/about">About US</Link>
+                            </li>
+                            <li className="text-white hover:text-indigo-200">
+                                <Link to="/contact">Contact US</Link>
+                            </li>
+                        </ul> */}
+
+                        <div className="mt-3 space-y-2 md:hidden inline-block">
+                            {/* <a
+                                Link
+                                to="/"
+                                className="inline-block w-full px-4 py-2 text-center text-white bg-gray-600 rounded-md shadow hover:bg-gray-800"
+                            >
+                                Sign in
+                            </a>
+                            <a
+                                Link
+                                to="/"
+                                className="inline-block w-full px-4 py-2 text-center text-gray-800 bg-white rounded-md shadow hover:bg-gray-100"
+                            >
+                                Sign up
+                            </a> */}
+                            <ul
+                                className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0"
+                                onClick={() => setNavbar(!navbar)}
+                            >
+                                <li className="text-white hover:text-indigo-200">
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li className="text-white hover:text-indigo-200">
+                                    <Link to="/write">Write</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="hidden space-x-2 md:inline-block">
+                    <ul
+                        className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0"
+                        onClick={() => setNavbar(!navbar)}
+                    >
+                        <li className="text-white hover:text-indigo-200">
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li className="text-white hover:text-indigo-200">
+                            <Link to="/write">Write</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
---
+++ src/Pages/Write/Write.jsx
@@ -0,0 +1,144 @@
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const Write = () => {
+    const [inputCats, setInputCats] = useState([{ cat: "" }]);
+    const addCats = (index, event) => {
+        const values = [...inputCats];
+        values[index][event.target.name] = event.target.value;
+        setInputCats(values);
+    };
+
+    // Add new cat fields
+    const addCatFilds = () => {
+        setInputCats([...inputCats, { cat: "" }]);
+    };
+    // Remove cat fields
+    const removeCatFilds = (index) => {
+        const values = [...inputCats];
+        values.splice(index, 1);
+        setInputCats(values);
+    };
+
+    // on submit handaler
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const title = form.title.value;
+        const desc = form.desc.value;
+        const image = form.files.files[0];
+
+        const imageBB = "d1445eeabe3574dc24041179a4e9edef";
+        const formData = new FormData();
+        formData.append("image", image);
+        const url = `https://api.imgbb.com/1/upload?key=${imageBB}`;
+
+        fetch(url, {
+            method: "POST",
+            body: formData,
+        })
+            .then((response) => response.json())
+            .then((result) => {
+                if (result.success) {
+                    const data = {
+                        title,
+                        desc,
+                        cat: inputCats,
+                        img: result.data.url,
+                    };
+                    console.log(data);
+                }
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    };
+    return (
+        <div className="container mx-auto">
+            <form onSubmit={handleSubmit}>
+                <div className="w-full space-y-1">
+                    <label
+                        htmlFor="title"
+                        className="block text-sm font-medium"
+                    >
+                        Title
+                    </label>
+                    <input
+                        type="text"
+                        name="title"
+                        id="title"
+                        className="border border-purple-700 px-3 py-2 w-full"
+                    />
+                </div>
+                <div className="w-full space-y-1 flex flex-wrap">
+                    <div>
+                        {inputCats.map((event, index) => (
+                            <div key={index}>
+                                <label
+                                    htmlFor="cat"
+                                    className="block text-sm font-medium"
+                                >
+                                    Category
+                                </label>
+                                <div className="flex items-center gap-5">
+                                    <input
+                                        type="text"
+                                        name="cat"
+                                        id="cat"
+                                        className="border border-purple-700 px-3 py-2 w-full"
+                                        onChange={(event) =>
+                                            addCats(index, event)
+                                        }
+                                    />
+                                    <AiOutlineMinus
+                                        onClick={() => removeCatFilds(index)}
+                                        className="text-2xl cursor-pointer"
+                                    />
+                                    <AiOutlinePlus
+                                        onClick={addCatFilds}
+                                        className="text-2xl cursor-pointer"
+                                    />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="w-full space-y-1">
+                    <label htmlFor="desc" className="block text-sm font-medium">
+                        Description
+                    </label>
+                    <textarea
+                        name="desc"
+                        id="desc"
+                        cols="30"
+                        rows="10"
+                        className="border border-purple-700 px-3 py-2 w-full"
+                    ></textarea>
+                </div>
+                <fieldset className="w-full space-y-1 dark:text-gray-100">
+                    <label
+                        htmlFor="files"
+                        className="block text-sm font-medium"
+                    >
+                        Attachments
+                    </label>
+                    <div className="flex">
+                        <input
+                            type="file"
+                            name="files"
+                            id="files"
+                            className="px-8 py-12 border-2 border-dashed rounded-md dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800"
+                        />
+                    </div>
+                </fieldset>
+                <input
+                    type="submit"
+                    value="Submit"
+                    className=" cursor-pointer bg-purple-500 px-5 py-3 mt-5 font-bold text-white"
+                />
+            </form>
+        </div>
+    );
+};
+
+export default Write;
